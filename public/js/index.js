41df078a4453b675b1fac7720f743bcfed94b38d
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-12-29 12:26:55 +0200

    smooth transitions between pages

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,19 @@
 /*** DOCUMENT READY ******************************************************************************/
 
 $(document).ready(function() {
+  firstPageSize();
+  var isSecondPageSizeSet = false;
+  $(document).on("pageshow","#mypage2",function() {
+    if (isSecondPageSizeSet == false) {
+      secondPageSize();
+      isSecondPageSizeSet = true;
+    }
+  });
+});
+
+/*************************************************************************************************/
+
+function firstPageSize() {
   /* Page height */
   var mycontentPadding = parseFloat($("#mycontent").css('padding'));
   var mycontentHeight  = parseFloat($("#mycontent").css('height'));
@@ -14,11 +27,32 @@ $(document).ready(function() {
 
   /* Top content width */
   var topcontentWidth = parseFloat($("#topcontent").css('width'));
-  var gorightbuttonWidth  = parseFloat($("#gorightbutton").css('width'));
-  var gorightbuttonMargin = parseFloat($("#gorightbutton").css('margin-left'));
-  $("#topcontentleft").css('width', topcontentWidth - gorightbuttonWidth -
-                                    2 * gorightbuttonMargin);
-  $("#topcontentright").css('width', gorightbuttonWidth + 2 * gorightbuttonMargin);
-});
+  var navbuttonWidth  = parseFloat($("#navbutton").css('width'));
+  var navbuttonMargin = parseFloat($("#navbutton").css('margin-left'));
+  $("#topcontentleft").css('width', topcontentWidth - navbuttonWidth -
+                                    2 * navbuttonMargin);
+  $("#topcontentright").css('width', navbuttonWidth + 2 * navbuttonMargin);
+}
 
-/*************************************************************************************************/
+
+function secondPageSize() {
+  /* Page height */
+  var mycontentPadding = parseFloat($("#mycontent2").css('padding'));
+  var mycontentHeight  = parseFloat($("#mycontent2").css('height'));
+  $("#mycontent2").css('height', mycontentHeight - 2 * mycontentPadding);
+
+  /* List height */
+  mycontentHeight      = parseFloat($("#mycontent2").css('height'));
+  var topcontentHeight = parseFloat($("#topcontent2").css('height'));
+  $("#mylist2").css('max-height', mycontentHeight - 2 * mycontentPadding -
+                                 topcontentHeight);
+
+  /* Top content width */
+  var topcontentWidth = parseFloat($("#topcontent2").css('width'));
+  var navbuttonWidth  = parseFloat($("#navbutton2").css('width'));
+  var navbuttonMargin = parseFloat($("#navbutton2").css('margin-left'));
+  $("#topcontentleft2").css('width', navbuttonWidth + 2 * navbuttonMargin);
+
+  $("#topcontentright2").css('width', topcontentWidth - navbuttonWidth -
+                                      2 * navbuttonMargin);
+}
