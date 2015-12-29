77b6ea29b5fefa8f8e38c2a70bd02c859c6fe204
Răzvan-Mădălin MATEI <matei.rm94@gmail.com>
2015-12-29 11:45:49 +0200

    add second page

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,24 @@
-/*** DOCUMENT READY **********************************************************/
+/*** DOCUMENT READY ******************************************************************************/
 
 $(document).ready(function() {
+  /* Page height */
   var mycontentPadding = parseFloat($("#mycontent").css('padding'));
   var mycontentHeight  = parseFloat($("#mycontent").css('height'));
-
   $("#mycontent").css('height', mycontentHeight - 2 * mycontentPadding);
-  mycontentHeight  = parseFloat($("#mycontent").css('height'));
 
-  var searchboxHeight = parseFloat($("#searchbox").css('height'));
-
-  var mylistMargin = parseFloat($("#mylist").css('margin'));
-
-  console.log(searchboxHeight);
-  console.log(mycontentHeight);
-  console.log(mycontentPadding);
-
+  /* List height */
+  mycontentHeight      = parseFloat($("#mycontent").css('height'));
+  var topcontentHeight = parseFloat($("#topcontent").css('height'));
   $("#mylist").css('max-height', mycontentHeight - 2 * mycontentPadding -
-                             searchboxHeight);
+                                 topcontentHeight);
+
+  /* Top content width */
+  var topcontentWidth = parseFloat($("#topcontent").css('width'));
+  var gorightbuttonWidth  = parseFloat($("#gorightbutton").css('width'));
+  var gorightbuttonMargin = parseFloat($("#gorightbutton").css('margin-left'));
+  $("#topcontentleft").css('width', topcontentWidth - gorightbuttonWidth -
+                                    2 * gorightbuttonMargin);
+  $("#topcontentright").css('width', gorightbuttonWidth + 2 * gorightbuttonMargin);
 });
 
-/*****************************************************************************/
+/*************************************************************************************************/
